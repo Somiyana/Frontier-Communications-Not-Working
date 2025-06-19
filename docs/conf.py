f2eb67165e066da4e9fe900d6b1f05e84108a98f
Somiyana <somiyanamir@gmail.com>
2025-06-19 11:47:51 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,10 @@
+project = 'Frontier Communications Not Working'
+author = 'Your Name'
+release = '1.0'
+
+extensions = []
+templates_path = ['_templates']
+exclude_patterns = []
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
